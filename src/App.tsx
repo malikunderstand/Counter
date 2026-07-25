f1f4c11e11d2d10b8384d7a3d9c1da6ad8524032
NazeerMalik <nazeermalik23456@gmail.com>
2026-07-25 14:16:41 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import 'boxicons/css/boxicons.min.css';
 import './App.css';
-import Navbar from './Navbar';
 import Counter from './Counter';
-import Footer from './Footer';
+
 
 function App() {
   return (
     <>
-      <Navbar />
       <Counter />
-      <Footer />
     </>
   );
 }
